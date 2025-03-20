481df72162ebd00d4f7a39c3628bcdef42ccdd96
ordell <henrikekholm1987@protonmail.com>
2025-03-20 13:59:46 +0100

    added gitignore

--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@ int main()
 {
     int num1 = 10;
     int num2 = 20;
+    int num3 = 610;
 
-    int sum = num1 + num2;
+    int sum = num1 + num2 + num3;
 
     std::cout <<"The sum of " << num1 << " and " << num2 << " is: " << sum << std::endl;
     return 0;
