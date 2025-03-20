5abccb537d304c7c97d8586143225a757eab7add
ordell <henrikekholm1987@protonmail.com>
2025-03-20 16:38:05 +0100

    important change

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main()
     int num1, num2;
 
     std::cout << "Choose your first (first = #1) variable: ";
+    std::cout << "Choose your first (first = #1) variable: ";
 
     std::cin >> num1;
 
